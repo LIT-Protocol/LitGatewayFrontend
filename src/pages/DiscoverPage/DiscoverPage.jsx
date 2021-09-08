3fbfefdad1f0c5cfd92c436436301a4576003380
Chris Cassano <glitch0@gmail.com>
2021-09-08 13:21:20 -0700

    cleaned up discover

--- src/pages/DiscoverPage/DiscoverPage.jsx
+++ src/pages/DiscoverPage/DiscoverPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import styles from './discover-page.module.scss'
 
@@ -8,11 +8,15 @@ import { Grid, GridItem } from '@consta/uikit/Grid'
 
 import { useAppContext } from '../../context'
 
-import Background from './assets/maksim-istomin-w6auPDgfDS0-unsplash.jpg'
-import Card1 from './assets/keming-tan-BouGISvED5w-unsplash.jpg'
+import Background from './assets/paul-gilmore-8kDOOrs608I-unsplash.jpg'
+import Card1 from './assets/dylan-mullins-Ubhjpv7q0Pk-unsplash.jpg'
+import Card2 from './assets/markus-winkler-cV9-hOgoaok-unsplash.jpg'
+import Card3 from './assets/kelly-sikkema-Kl1gC0ve620-unsplash.jpg'
+import Card4 from './assets/james-harrison-vpOeXr5wmR4-unsplash.jpg'
 
 const DiscoverPage = () => {
   const { performWithAuthSig } = useAppContext()
+  const history = useHistory()
 
   const handleConnectWallet = () => {
     performWithAuthSig((authSig) => {
@@ -27,10 +31,11 @@ const DiscoverPage = () => {
         style={{ backgroundImage: `url(${Background})` }}
       >
         <div className={styles.content}>
-          <h2>Lit Gateway - token gated experiences</h2>
+          <h2>Lit Gateway</h2>
           <h1 className={styles.title} as="h1" inverted>
-            Find your tribe <br /> and playground
+            Token gated experiences
           </h1>
+          <h4 className={styles.subtitle}>Powered by the 🔥 Lit Protocol</h4>
         </div>
         <Button label="Connect wallet" size="l" onClick={handleConnectWallet} />
       </div>
@@ -48,8 +53,8 @@ const DiscoverPage = () => {
           <div className={styles.smallCard}>
             <div>
               <h4>FEATURED</h4>
-              <h2 className={styles.title}>ETH Whales Beat Saber</h2>
-              <h5>Compete for ETH and NFTs</h5>
+              <h2 className={styles.title}>Minter</h2>
+              <h5>Put locked content behind NFTs</h5>
             </div>
             <img className={styles.image} src={Card1} alt={Card1} />
           </div>
@@ -58,16 +63,16 @@ const DiscoverPage = () => {
           <div className={styles.smallCard}>
             <div>
               <h4>FEATURED</h4>
-              <h2 className={styles.title}>ETH Whales Beat Saber</h2>
-              <h5>Compete for ETH and NFTs</h5>
+              <h2 className={styles.title}>Files</h2>
+              <h5>Collaborative Decentralized Encrypted File Storage</h5>
             </div>
-            <img className={styles.image} src={Card1} alt={Card1} />
+            <img className={styles.image} src={Card2} alt={Card2} />
           </div>
         </GridItem>
       </Grid>
       <div className={styles.block}>
-        <h2 className={styles.title}>Experiences we love</h2>
-        <Link to="/">See all</Link>
+        <h2 className={styles.title}>Learn more about the Lit Gateway</h2>
+        {/* <Link to="/">See all</Link> */}
       </div>
       <Grid
         cols="1"
@@ -82,15 +87,34 @@ const DiscoverPage = () => {
         <GridItem>
           <div className={styles.miniCard}>
             <div>
-              <img src={Card1} alt="" />
-              <Link to="/app">
-                <h2 className={styles.title}>Yield Farming Chat</h2>
+              <img src={Card3} alt="" />
+              <Link to="/guide">
+                <h2 className={styles.title}>Guide</h2>
               </Link>
             </div>
-            <Button label="Get" size="s" />
+            <Button
+              label="View"
+              size="s"
+              onClick={() => history.push('/guide')}
+            />
           </div>
         </GridItem>
         <GridItem>
+          <div className={styles.miniCard}>
+            <div>
+              <img src={Card4} alt="" />
+              <Link to="/build">
+                <h2 className={styles.title}>Build</h2>
+              </Link>
+            </div>
+            <Button
+              label="View"
+              size="s"
+              onClick={() => history.push('/build')}
+            />
+          </div>
+        </GridItem>
+        {/* <GridItem>
           <div className={styles.miniCard}>
             <div>
               <img src={Card1} alt="" />
@@ -111,18 +135,7 @@ const DiscoverPage = () => {
             </div>
             <Button label="Get" size="s" />
           </div>
-        </GridItem>
-        <GridItem>
-          <div className={styles.miniCard}>
-            <div>
-              <img src={Card1} alt="" />
-              <Link to="/app">
-                <h2 className={styles.title}>Yield Farming Chat</h2>
-              </Link>
-            </div>
-            <Button label="Get" size="s" />
-          </div>
-        </GridItem>
+        </GridItem> */}
       </Grid>
     </div>
   )
