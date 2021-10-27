ef595fdf3d8d1c6dd5104ca44be903771ffa0141
Nick <forizual@gmail.com>
2021-10-27 12:46:23 +0300

    Center cards under rest of content on Discover page

--- src/pages/DiscoverPage/DiscoverPage.jsx
+++ src/pages/DiscoverPage/DiscoverPage.jsx
@@ -3,14 +3,12 @@ import { useHistory } from 'react-router-dom'
 
 import styles from './discover-page.module.scss'
 
-import bg from './assets/bg.png'
 import discountLogo from './assets/discount-offer-icon.png'
 import discountBack from './assets/discountBack.png'
 import litLogo from './assets/lit-offer-icon.png'
 import litBack from './assets/litBack.png'
 
 import { Button } from '@consta/uikit/Button'
-import { Grid, GridItem } from '@consta/uikit/Grid'
 import { Modal } from '@consta/uikit/Modal'
 import { IconClose } from '@consta/uikit/IconClose'
 
@@ -56,7 +54,6 @@ const DiscoverPage = () => {
   return (
     <div className={styles.main}>
       <div className={styles.top}>
-        <img className={styles.bg} src={bg} alt="" />
         <div className={styles.title}>
           <h3>Discover</h3>
           <h2>Lit Gateway</h2>
@@ -88,86 +85,71 @@ const DiscoverPage = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <Grid
-          cols="1"
-          gap="xl"
-          breakpoints={{
-            l: {
-              cols: 3,
-            },
-            s: {
-              cols: 2,
-            },
-          }}
-        >
-          <GridItem>
-            <Card
-              title="InsureAce Discount"
-              titleIcon={discountLogo}
-              tags={['Staking', 'Finance']}
-              className={styles.offer}
-              btns={
-                <Button
-                  size="l"
-                  label="Details"
-                  onClick={() => handleOpenOffer('insureace-discount')}
-                />
-              }
-              desc={
-                <div className={styles.descList}>
-                  <ul>
-                    <li>
-                      REQUIREMENT: <span>Yield farmer</span>
-                    </li>
-                    <li>
-                      REWARD: <span>30% off for 1 month</span>
-                    </li>
-                  </ul>
-                </div>
-              }
-              img={discountBack}
+        <Card
+          title="InsureAce Discount"
+          titleIcon={discountLogo}
+          tags={['Staking', 'Finance']}
+          className={styles.offer}
+          btns={
+            <Button
+              size="l"
+              label="Details"
+              onClick={() => handleOpenOffer('insureace-discount')}
             />
-          </GridItem>
-          <GridItem>
-            <Card
-              title="Lit Protocol NFT Drop 01"
-              titleIcon={litLogo}
-              tags={['Lit Protocol']}
-              className={styles.offer}
-              btns={
-                <Button
-                  size="l"
-                  label="Details"
-                  onClick={() => handleOpenOffer('lit-protocol-nft')}
-                />
-              }
-              desc={
-                <div className={styles.descList}>
-                  <ul>
-                    <li>
-                      REQUIREMENT:{' '}
-                      <span>
-                        Follow{' '}
-                        <a
-                          className={styles.link}
-                          href="https://twitter.com/litprotocol"
-                          target="_blank"
-                        >
-                          @LitProtocol on Twitter
-                        </a>
-                      </span>
-                    </li>
-                    <li>
-                      REWARD: <span>NFT</span>
-                    </li>
-                  </ul>
-                </div>
-              }
-              img={litBack}
+          }
+          desc={
+            <div className={styles.descList}>
+              <ul>
+                <li>
+                  REQUIREMENT: <span>Yield farmer</span>
+                </li>
+                <li>
+                  REWARD: <span>30% off for 1 month</span>
+                </li>
+              </ul>
+            </div>
+          }
+          img={discountBack}
+        />
+
+        <Card
+          title="Lit Protocol NFT Drop 01"
+          titleIcon={litLogo}
+          tags={['Lit Protocol']}
+          className={styles.offer}
+          btns={
+            <Button
+              size="l"
+              label="Details"
+              onClick={() => handleOpenOffer('lit-protocol-nft')}
             />
-          </GridItem>
-        </Grid>
+          }
+          desc={
+            <div className={styles.descList}>
+              <ul>
+                <li>
+                  REQUIREMENT:{' '}
+                  <span>
+                    Follow{' '}
+                    <a
+                      className={styles.link}
+                      href="https://twitter.com/litprotocol"
+                      target="_blank"
+                    >
+                      @LitProtocol on Twitter
+                    </a>
+                  </span>
+                </li>
+                <li>
+                  REWARD: <span>NFT</span>
+                </li>
+              </ul>
+            </div>
+          }
+          img={litBack}
+        />
       </div>
+
       <Modal isOpen={showingEmailCaptureModal}>
         <div className={styles.emailCaptureModal}>
           <div className={styles.closeHolder}>
