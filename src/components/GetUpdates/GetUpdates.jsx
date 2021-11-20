6fd39be96898025e3fe9bd5641d301242f646da5
Zane Russell <zane@zanes-mbp.lan>
2021-11-20 13:55:02 -0700

    make email signup message responsive

--- src/components/GetUpdates/GetUpdates.jsx
+++ src/components/GetUpdates/GetUpdates.jsx
@@ -1,78 +1,22 @@
-import { useState } from 'react'
-import cx from 'classnames'
-
-import { Button } from '@consta/uikit/Button'
-import { InputWrapper } from '../../components'
-
-import styles from './get-updates.module.scss'
 import MailchimpSubscribe from './MailchimpSubscribe'
-import { useAppContext } from '../../context'
-import { putUser } from '../../api/users'
+
+import CustomForm from './CustomForm'
 
 const GetUpdates = (props) => {
-  const { validateEmail } = useAppContext()
   const { className } = props
-
-  const [emailVal, setEmailVal] = useState('')
-  const [added, setAdded] = useState(false)
-  const [errorStatus, setErrorStatus] = useState(false)
-  const [inputMessage, setInputMessage] = useState(false)
 
   const addUrl =
     'https://litprotocol.us20.list-manage.com/subscribe/post?u=37eeb6bbc8f1fee01838fd9df&id=4f37de62dc'
-
-  const CustomForm = ({ status, message, onValidated }) => {
-    const submit = async () => {
-      if (!validateEmail(emailVal)) {
-        setErrorStatus('error')
-        setInputMessage('Email is invalid')
-        return
-      }
-
-      if (!added) {
-        setErrorStatus('regular')
-        setInputMessage(null)
-        await putUser({
-          email: emailVal,
-          authSig: null,
-        })
-        setAdded(true)
-      }
-
-      onValidated({
-        EMAIL: emailVal,
-      })
-    }
-
-    return (
-      <div className={cx(styles.form, className)}>
-        <InputWrapper
-          value={emailVal}
-          className={styles.input}
-          placeholder="Email address"
-          error={inputMessage}
-          id="email"
-          size="l"
-          status={errorStatus}
-          handleChange={(value) => setEmailVal(value)}
-        />
-        <Button
-          className={cx(styles.btn, added && styles.added)}
-          label={!added ? 'Get Updates' : 'Added!'}
-          size="l"
-          onClick={submit}
-        />
-      </div>
-    )
-  }
 
   return (
     <div>
       <MailchimpSubscribe
         url={addUrl}
         render={({ subscribe, status, message }) => {
+          console.log('message', message)
           return (
             <CustomForm
+              className={className}
               status={status}
               message={message}
               onValidated={(formData) => subscribe(formData)}
