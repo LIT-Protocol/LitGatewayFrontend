36756ca8b7b13d232cb5b64557138330d878840a
zanedr <zanedanielrussell@gmail.com>
2022-01-26 14:58:17 -0700

    update users calls

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,3 @@
-const SibApiV3Sdk = require('sib-api-v3-typescript')
-
 const apiUrl = process.env.REACT_APP_LIT_GATEWAY_FRONTEND_API_URL
 const emailApi = process.env.REACT_APP_SEND_IN_BLUE_API
 
@@ -15,18 +13,14 @@ export const putUser = (body) => {
 }
 
 export const postUser = (body) => {
-  let apiInstance = new SibApiV3Sdk.ContactsApi()
-
-  let apiKey = apiInstance.authentications['apiKey']
-
-  apiKey.apiKey = emailApi
-
-  let createContact = new SibApiV3Sdk.CreateContact()
-
-  createContact.email = body.email
-  createContact.listIds = [2]
-
-  return apiInstance.createContact(createContact)
+  return fetch(apiUrl + '/saveSubscribe', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then((response) => response.json())
 }
 
 export const getUserHoldings = (body) => {
